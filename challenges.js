73bf4d28374fd5debcd583403fd74edca96edffd
DavidStinson <mail@davidstinson.dev>
2020-11-01 01:00:49 -0500

    make change to d4

--- challenges.js
+++ challenges.js
@@ -90,7 +90,7 @@ Difficulty: Basic
 
 Prompt:
 
-- Write a function called addList that accepts any quantity of numbers as arguments, adds them together and returns the resulting sum.
+- Write a function called addList that accepts any quantity of numbers as arguments, adds them together, and returns the resulting sum.
 - Assume all parameters will be numbers.
 - If called with no arguments, return 0 (zero).
 
