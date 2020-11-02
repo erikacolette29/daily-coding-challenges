4f9232511b7a7ddf98d98502f74bc2945cc2ae1c
erikacolette <erikatidwell.c@gmail.com>
2020-11-02 16:13:24 -0500

    solve challenge 1

--- challenges.js
+++ challenges.js
@@ -33,6 +33,9 @@ addOne(-5) //=> -4
 -----------------------------------------------------------------------------*/
 // Your solution for 01-addOne here:
 
+function addOne(num) {
+  return num +1;
+}
 
 
 
