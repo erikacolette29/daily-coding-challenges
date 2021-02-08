03c265ddccf144339b8b8fd7bf7be0b714c2958c
erikacolette <erikatidwell.c@gmail.com>
2021-02-07 21:38:11 -0500

    30

--- challenges.js
+++ challenges.js
@@ -1197,22 +1197,18 @@ totalTaskTime( [5, 2, 6, 8, 7, 2], 3 ) // => 12
 function totalTaskTime(tasks, numThreads) {
   var time = 0, shortest, threads;
   while(tasks.length > numThreads) {
-    // extract a task for each thread
+   
     threads = tasks.splice(0, numThreads);
-    // Find out the time for the task that will finish first.
-    // Using the spread operator to provide Math.min with a list of values
+   
     shortest = Math.min(...threads);
-    // Add the time for that shortest task
+   
     time += shortest;
-    // Reduce each task in threads by the shortest task time and
-    // remove all of those completed "short" tasks
+ 
     threads = threads.map(t => t - shortest).filter(t => t);
-    // Put any remaining tasks back into threads and do it again (loop)...
+   
     tasks = threads.concat(tasks);
   }
-  // When num remaining tasks is less or equal to numThreads,
-  // we just need to add the time from the longest remaining task.
-  // The ternary protects against Math.max returning infinity on an empty array
+
   return time + (tasks.length ? Math.max(...tasks) : 0);
 }
 
