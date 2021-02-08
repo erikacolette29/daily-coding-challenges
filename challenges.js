7d3443284ea52cb5b2a36aab378500766115a346
erikacolette <erikatidwell.c@gmail.com>
2021-02-07 21:37:29 -0500

    30 done but no clue, need to study

--- challenges.js
+++ challenges.js
@@ -1173,12 +1173,47 @@ totalTaskTime( [4, 2, 10], 2 ) // => 12
 totalTaskTime( [2, 2, 3, 3, 4, 4], 2 ) //=> 9
 totalTaskTime( [5, 2, 6, 8, 7, 2], 3 ) // => 12
 -----------------------------------------------------------------------------*/
-// Your solution for 30- here:
+// Your solution for 30- here: NOT A CLUE STUDY!! 
 
-function totalTaskTime(queue, threads){
-  let totalTime = 0
+// function totalTaskTime(queues, nThreads){
+//   let totalTime = 0, short, threads
+//   while(queues.length > nThreads){
+//     threads = queues.splice(0, nThreads)
+//     short = Math.min(...threads)
+//     totalTime += short
+//     threads = threads.map(t => t - short).filter(t => t)
+//     queues = threads.concat(queues)
+//   }
 
 
-  return totalTime
+//   return totalTime + (queues.length ? Math.max(...queues) : 0);
 
+// }
+
+// totalTaskTime( [2, 2, 3, 3, 4, 4], 2 )
+// totalTaskTime( [], 1 )
+
+
+function totalTaskTime(tasks, numThreads) {
+  var time = 0, shortest, threads;
+  while(tasks.length > numThreads) {
+    // extract a task for each thread
+    threads = tasks.splice(0, numThreads);
+    // Find out the time for the task that will finish first.
+    // Using the spread operator to provide Math.min with a list of values
+    shortest = Math.min(...threads);
+    // Add the time for that shortest task
+    time += shortest;
+    // Reduce each task in threads by the shortest task time and
+    // remove all of those completed "short" tasks
+    threads = threads.map(t => t - shortest).filter(t => t);
+    // Put any remaining tasks back into threads and do it again (loop)...
+    tasks = threads.concat(tasks);
+  }
+  // When num remaining tasks is less or equal to numThreads,
+  // we just need to add the time from the longest remaining task.
+  // The ternary protects against Math.max returning infinity on an empty array
+  return time + (tasks.length ? Math.max(...tasks) : 0);
 }
+
+totalTaskTime( [2, 2, 3, 3, 4, 4], 2 )
